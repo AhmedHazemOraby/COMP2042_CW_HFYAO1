1b3173f4b9a9cca0452079e4325e61fbc3860ec3
AhmedHazemOraby <hfyao1@nottingham.edu.my>
2023-12-20 12:29:22 +0800

    Fixed placement of Java Docs comments

--- src/main/java/brickGame/Bonus.java
+++ src/main/java/brickGame/Bonus.java
@@ -5,11 +5,19 @@ import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import java.io.Serializable;
 import java.util.Random;
+
 /**
  * This class represents a bonus item in the brick game.
  * It manages the properties and appearance of the bonus, such as its position and image.
  */
 public class Bonus implements Serializable {
+
+    public Rectangle choco;
+    public double x;
+    public double y;
+    public long timeCreated;
+    public boolean taken = false;
+
     /**
      * Constructs a new Bonus at the specified row and column.
      * The position is calculated based on the dimensions and padding of the blocks.
@@ -17,17 +25,13 @@ public class Bonus implements Serializable {
      * @param row    The row position where the bonus will appear.
      * @param column The column position where the bonus will appear.
      */
-    public Rectangle choco;
-    public double x;
-    public double y;
-    public long timeCreated;
-    public boolean taken = false;
     public Bonus(int row, int column) {
         x = (column * (Block.getWidth())) + Block.getPaddingH() + (Block.getWidth() / 2) - 15;
         y = (row * (Block.getHeight())) + Block.getPaddingTop() + (Block.getHeight() / 2) - 15;
 
         draw();
     }
+
     /**
      * Initializes and draws the bonus item.
      * Sets the size, position, and image of the bonus.
